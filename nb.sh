f5a52ec1de4132d37593079242984d9383617d3d
brianaddicks <brian@addicks.us>
2023-12-01 10:19:31 -0500

    update nautobot_populate

--- nb.sh
+++ nb.sh
@@ -2,4 +2,4 @@
 ansible-navigator run ./playbooks/nautobot_populate.yml \
   --penv NAUTOBOT_URL \
   --penv NAUTOBOT_TOKEN \
-  --eei quay.io/rh_ee_baddicks/nautobot-ee
+  --eei quay.io/rh_ee_baddicks/nautobot-ee -m stdout
